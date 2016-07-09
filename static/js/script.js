f50c474c1660aeac20c233794b03fa0773d6e491
mac <nicholasdwoodward@gmail.com>
2016-07-08 23:55:46 -0400

    mobile

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,28 @@
+var supportsAutoplay = false;
+var waitTime;
+
 $('#bg-video').vide({
 	mp4: 'static/media/video/comingsoon.mp4',
-	poster: 'static/media/img/snow.jpg',
+	poster: 'static/media/img/pv_still.jpg',
 }, {
 	posterType: 'jpg',
 	origin: 'content-box',
 });
 instance = $('#bg-video').data('vide');
 video = instance.getVideoObject();
-video.onloadeddata = function(){
+video.addEventListener('play', function () {
+    supportsAutoplay = true;
+});
+setTimeout(          //wait for listener to run
+    function(){
+        if(supportsAutoplay){
+        	video.onloadeddata = function(){
 	$('#bg-video-box').addClass('shown');
 }
+        }
+        else {
+        	$('#bg-video-box').addClass('shown');
+        }
+    },
+    waitTime
+); 
